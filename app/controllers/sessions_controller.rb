c8a29c15e618438040ee9143b20281fc2e8fe759
Andela Stevanovic <andjela.s166@gmail.com>
2022-08-24 18:45:09 -0400

    commit tree 10743c90c4a5133ce47d64f415ab5d299085af0e

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def create 
+        user = User.find_by(username: params[:username])
+        is user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+
+    else
+        render json: { error: "Invalid username or password" }, status: :unauthorized
+    end
+end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
+# Create a Sessions controller with a create action for logging in
+#  that responds to a POST /login request, 
+#  and a destroy action for logging out that responds to a DELETE /logout request.
